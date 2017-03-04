5bb4a10604050dd00f0eac39d84144adfd64406e
April Daley <daley.april@gmail.com>
2017-03-04 15:26:01 -0500

    Adding table and style sheet

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
+import csv 
 from flask import Flask 
 from flask import render_template
 app = Flask(__name__)
 
+
+def get_csv():
+	csv_path = "static/la-riots-deaths.csv"
+	# The code below tells Python to open the file and read it, instead of the default open and write aka delete
+	csv_file = open(csv_path, 'rb')
+	# This calls a method that organizes the data
+	csv_obj = csv.DictReader(csv_file)
+	csv_list = list(csv_obj)
+	return csv_list
+
+
 @app.route('/')
 def index():
 	template = "index.html"
-	return render_template(template)
+	object_list = get_csv()
+	return render_template(template, object_list=object_list)
 
 # If this script is run from the command line...
 if __name__ == "__main__":
